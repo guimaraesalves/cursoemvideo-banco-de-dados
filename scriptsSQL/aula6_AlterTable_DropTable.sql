8f2cd6e1d04f08e4e5f88349408954e36941b7d1
Mateus Guimarães Alves <guimaraesalves45@gmail.com>
2021-02-20 00:19:02 -0300

    Update aula6_AlterTable_DropTable.sql

--- scriptsSQL/aula6_AlterTable_DropTable.sql
+++ scriptsSQL/aula6_AlterTable_DropTable.sql
@@ -29,6 +29,9 @@ VALUES
 
 desc peoples;
 
+/* AULA 6 - Alterando a Estrutura da
+Tabela (ALTER TABLE E DROP TABLE */
+
 alter table pessoas
 add column profissao varchar(10);
 
@@ -67,6 +70,53 @@ ADD COLUMN idcurso int FIRST;
 
 ALTER TABLE cursos
 ADD PRIMARY KEY(idcurso);
+
+/* AULA 7 - Manipulando Linhas
+(UPDATE, DELETE E TRUNCATE) */
+
+INSERT INTO cursos values
+('1','HTML4','Curso de HTML5','40','37','2014'),
+('2','Algoritmos','Lógica de Programação','20','15','2014'),
+('3','Photoshop','Dicas de PhotoShop CC','10','8','2014'),
+('4','PGP','Curso de PHP para iniciantes','40','20','2010'),
+('5','Jarva','Introdução à Linguagem Java','10','29','2000'),
+('6','MySQL','Bancos de Dados MySQL','40','15','2016'),
+('7','Word','Curso completo de Word','40','30','2016'),
+('8','Sapateado','Danças Rítmicas','40','30','2022'),
+('9','Cozinha Árabe','Aprenda a fazer kibe','40','37','2022'),
+('10','YouTuber','Gerar polêmica e ganhar inscritos','5','37','2022');
+
+select * from cursos;
+
+-- MODIFICANDO LINHAS INCORRETAS
+UPDATE cursos
+SET nome = 'HTML5'
+WHERE idcurso = '1';
+
+UPDATE cursos
+SET nome = 'PHP', ano = '2015'
+WHERE idcurso = '4';
+
+UPDATE cursos
+SET nome = 'Java', ano = '2015', carga = '40'
+WHERE idcurso = '5'
+limit 1;
+
+DELETE FROM cursos
+WHERE idcurso = '8';
+
+DELETE FROM cursos
+WHERE idcurso = '9';
+
+DELETE FROM cursos
+WHERE idcurso = '10';
+
+
+
+
+
+
+
 
 
 
